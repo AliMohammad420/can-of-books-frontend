80be5437c6f315fadef868d3efc22d9cfe3eea56
AliMohammad420 <aliwessam509@gmail.com>
2022-08-08 17:02:22 +0300

    Lab15

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Navbar, NavItem } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import AuthButtons from './Auth/AuthButtons';
+import { useAuth0 } from '@auth0/auth0-react';
 
-class Header extends React.Component {
-  render() {
+function Header(){
+  const { isAuthenticated } = useAuth0();
+ 
     return (
+      
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
         <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
-        {/* PLACEHOLDER: render a navigation link to the about page */}
+        {isAuthenticated &&
+          <NavItem><Link to="/profile" className="nav-link">User Profile</Link></NavItem>
+        }
+        <AuthButtons className={'Auth'} />
       </Navbar>
+      
     )
   }
-}
+
 
 export default Header;
